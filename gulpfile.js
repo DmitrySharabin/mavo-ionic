9f7ac6a61c21237a4ff5f18570169729f3d5801d
Dmitry Sharabin <dmitrysharabin@gmail.com>
2020-03-13 16:54:22 +0300

    Init commit

---
+++ gulpfile.js
@@ -0,0 +1,100 @@
+const gulp = require("gulp");
+const babel = require("gulp-babel");
+const minify = require("gulp-babel-minify");
+const injectVersion = require("gulp-inject-version");
+const rename = require("gulp-rename");
+
+const versionOptions = {
+	replace: /%%VERSION%%/g
+};
+
+gulp.task("inject-version", function() {
+	return gulp
+		.src("src/mavo-ionic.js")
+		.pipe(injectVersion(versionOptions))
+		.pipe(gulp.dest("dist"));
+});
+
+gulp.task("transpile", function() {
+	return gulp
+		.src("src/mavo-ionic.js")
+		.pipe(
+			babel({
+				presets: [
+					[
+						"@babel/env",
+						{
+							targets: {
+								browsers: ["last 4 versions", "IE 11"]
+							}
+						}
+					]
+				],
+				compact: false
+			})
+		)
+		.pipe(injectVersion(versionOptions))
+		.pipe(rename({ extname: ".es5.js" }))
+		.pipe(gulp.dest("dist"));
+});
+
+gulp.task("minify", function() {
+	return gulp
+		.src("src/mavo-ionic.js")
+		.pipe(minify())
+		.pipe(rename({ extname: ".min.js" }))
+		.pipe(gulp.dest("dist"));
+});
+
+gulp.task("minify-es5", function() {
+	return gulp
+		.src("src/mavo-ionic.js")
+		.pipe(
+			babel({
+				presets: [
+					[
+						"@babel/env",
+						{
+							targets: {
+								browsers: ["last 4 versions", "IE 11"]
+							}
+						}
+					]
+				],
+				compact: false
+			})
+		)
+		.pipe(minify())
+		.pipe(rename({ extname: ".es5.min.js" }))
+		.pipe(gulp.dest("dist"));
+});
+
+gulp.task("default", function() {
+	return gulp
+		.src("src/mavo-ionic.js")
+		.pipe(
+			babel({
+				presets: [
+					[
+						"@babel/env",
+						{
+							targets: {
+								browsers: ["last 4 versions", "IE 11"]
+							}
+						}
+					]
+				],
+				compact: false
+			})
+		)
+		.pipe(minify())
+		.pipe(gulp.dest("."));
+});
+
+exports.build = gulp.parallel(
+	"default",
+	"inject-version",
+	"transpile",
+	"minify",
+	"minify-es5"
+);
