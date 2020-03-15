238349d846e0d265178fed454413ddcef6ece403
Dmitry Sharabin <dmitrysharabin@gmail.com>
2020-03-15 11:25:55 +0300

    Add basic structure

---
+++ src/mavo-ionic.js
@@ -0,0 +1,15 @@
+//@ts-check
+
+/**
+ * Ionic Framework plugin for Mavo
+ * @author Dmitry Sharabin and contributors
+ * @version %%VERSION%%
+ */
+
+(function($, $$) {
+	"use strict";
+
+	Mavo.Plugins.register("ionic", {});
+
+	Mavo.Elements.register({});
+})(Bliss, Bliss.$$);
